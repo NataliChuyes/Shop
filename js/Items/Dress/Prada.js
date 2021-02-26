6c7ad1f0faaf154b5a683e8d0b3462d9e9508fdb
NataliChuyes <natalichuyes@gmail.com>
2021-02-26 12:47:34 +0300

    edit dress

--- js/Items/Dress/Prada.js
+++ js/Items/Dress/Prada.js
@@ -1,5 +1,26 @@
 'use strict';
-
+$(document).ready(function(){
+    $("#slider").owlCarousel();
+  });
+$(document).ready(function(){
+    const slider = $("#slider").owlCarousel({
+        items:4,
+        loop:true,
+        margin:1,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+});
 var xhr = new XMLHttpRequest(); //созд-е obj XMLHttpRequest
 xhr.open('GET', '/data/dress.json', false); //Инициализация запроса: GET - полуить данные, URL, синхронно
 xhr.send(); // отправка
@@ -11,29 +32,21 @@ if (xhr.status != 200) {
 
 
     function outputGoods(DATA) {
-        console.log(DATA)
-
-        document.getElementById('clarks').innerHTML = DATA.map(n => `
-        <div class="col-sm-1">
-        </div>
-            <div class="col-sm-4 picture">
-                <p class=""><img src="${n.ImageSource}" alt="No fragments" width="100%"></p>
+        document.getElementById('prada').innerHTML = DATA.map(n => `
+            <div class="col-sm-4">
+                <img src="${n.ImageSourceBig}" alt="No fragments" class="pictureDress">
             </div>
-            <div class="col-sm-6 characteristics">
+            <div class="col-sm-7 characteristics">
                 <p class="name">${n.name} </p>
                 <p class="">Date: <i>${n.date}</i></p>
                 <p class="">Price: <b>${n.cost}$</b></i></p>
                 <p class="">Country: ${n.country}</i></p>
-                <p class="">Sizes: ${n.sizeB}</i></p>
+                <p class="">Sizes: ${n.sizeD}</i></p>
                 <p class="">In stock: ${n.check}</i></p>
                 <p class="">Colors: ${n.color}</i></p><br>
                 <div class="description">
                     <p >Description: ${n.description}</i></p>
                 </div>
-            </div>
-            
-            <div class="col-sm-12 foot" style="background-color: #ccc ">
-                <p class="">Slider </p>
             </div>`).join('');
 
     }
